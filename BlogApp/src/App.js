395ba7f74b2273333913c16f44007f14ab12acc0
Saksham Agrawal <117618569+5ak5ham@users.noreply.github.com>
2024-07-30 16:23:38 +0530

    Update App.js

--- BlogApp/src/App.js
+++ BlogApp/src/App.js
@@ -13,9 +13,6 @@ function createRandomPost() {
 function App() {
   const [isFakeDark, setIsFakeDark] = useState(false);
 
-  // Derived state. These are the posts that will actually be displayed
-
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
       document.documentElement.classList.toggle("fake-dark-mode");
@@ -141,9 +138,8 @@ function List() {
 
 function Archive() {
   const { onAddPost } = usePost();
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+
   const [posts] = useState(() =>
-    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
